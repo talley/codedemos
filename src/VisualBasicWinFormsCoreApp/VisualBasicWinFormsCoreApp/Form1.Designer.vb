63d2e168a61d56904067ffba58bdc521ac3599c5
Talley Ouro <talleyouro@outlook.com>
2022-05-31 14:52:41 -0400

    Add EF Core project and working on DataGridView Double click event

--- src/VisualBasicWinFormsCoreApp/VisualBasicWinFormsCoreApp/Form1.Designer.vb
+++ src/VisualBasicWinFormsCoreApp/VisualBasicWinFormsCoreApp/Form1.Designer.vb
@@ -22,10 +22,171 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblcreatedat = New System.Windows.Forms.Label()
+        Me.lblcreatedby = New System.Windows.Forms.Label()
+        Me.txtid = New System.Windows.Forms.TextBox()
+        Me.txtemail = New System.Windows.Forms.TextBox()
+        Me.txtpass = New System.Windows.Forms.TextBox()
+        Me.txtcreatedat = New System.Windows.Forms.TextBox()
+        Me.txtcreatedby = New System.Windows.Forms.TextBox()
+        Me.btnadd = New System.Windows.Forms.Button()
+        Me.btnclear = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToOrderColumns = True
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 12)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 51
+        Me.DataGridView1.RowTemplate.Height = 29
+        Me.DataGridView1.Size = New System.Drawing.Size(1063, 639)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(1098, 75)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(46, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Email"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(1098, 135)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(70, 20)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Password"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(1098, 12)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(22, 20)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Id"
+        '
+        'lblcreatedat
+        '
+        Me.lblcreatedat.AutoSize = True
+        Me.lblcreatedat.Location = New System.Drawing.Point(1098, 195)
+        Me.lblcreatedat.Name = "lblcreatedat"
+        Me.lblcreatedat.Size = New System.Drawing.Size(80, 20)
+        Me.lblcreatedat.TabIndex = 4
+        Me.lblcreatedat.Text = "Created At"
+        '
+        'lblcreatedby
+        '
+        Me.lblcreatedby.AutoSize = True
+        Me.lblcreatedby.Location = New System.Drawing.Point(1098, 248)
+        Me.lblcreatedby.Name = "lblcreatedby"
+        Me.lblcreatedby.Size = New System.Drawing.Size(81, 20)
+        Me.lblcreatedby.TabIndex = 5
+        Me.lblcreatedby.Text = "Created By"
+        '
+        'txtid
+        '
+        Me.txtid.Location = New System.Drawing.Point(1175, 12)
+        Me.txtid.Name = "txtid"
+        Me.txtid.ReadOnly = True
+        Me.txtid.Size = New System.Drawing.Size(349, 27)
+        Me.txtid.TabIndex = 6
+        '
+        'txtemail
+        '
+        Me.txtemail.Location = New System.Drawing.Point(1175, 72)
+        Me.txtemail.Name = "txtemail"
+        Me.txtemail.Size = New System.Drawing.Size(349, 27)
+        Me.txtemail.TabIndex = 7
+        '
+        'txtpass
+        '
+        Me.txtpass.Location = New System.Drawing.Point(1175, 132)
+        Me.txtpass.Name = "txtpass"
+        Me.txtpass.Size = New System.Drawing.Size(349, 27)
+        Me.txtpass.TabIndex = 8
+        '
+        'txtcreatedat
+        '
+        Me.txtcreatedat.Location = New System.Drawing.Point(1175, 192)
+        Me.txtcreatedat.Name = "txtcreatedat"
+        Me.txtcreatedat.ReadOnly = True
+        Me.txtcreatedat.Size = New System.Drawing.Size(349, 27)
+        Me.txtcreatedat.TabIndex = 9
+        '
+        'txtcreatedby
+        '
+        Me.txtcreatedby.Location = New System.Drawing.Point(1175, 241)
+        Me.txtcreatedby.Name = "txtcreatedby"
+        Me.txtcreatedby.ReadOnly = True
+        Me.txtcreatedby.Size = New System.Drawing.Size(349, 27)
+        Me.txtcreatedby.TabIndex = 10
+        '
+        'btnadd
+        '
+        Me.btnadd.Location = New System.Drawing.Point(1175, 284)
+        Me.btnadd.Name = "btnadd"
+        Me.btnadd.Size = New System.Drawing.Size(155, 29)
+        Me.btnadd.TabIndex = 11
+        Me.btnadd.Text = "Add User"
+        Me.btnadd.UseVisualStyleBackColor = True
+        '
+        'btnclear
+        '
+        Me.btnclear.Location = New System.Drawing.Point(1369, 284)
+        Me.btnclear.Name = "btnclear"
+        Me.btnclear.Size = New System.Drawing.Size(155, 29)
+        Me.btnclear.TabIndex = 12
+        Me.btnclear.Text = "Clear Form"
+        Me.btnclear.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1536, 663)
+        Me.Controls.Add(Me.btnclear)
+        Me.Controls.Add(Me.btnadd)
+        Me.Controls.Add(Me.txtcreatedby)
+        Me.Controls.Add(Me.txtcreatedat)
+        Me.Controls.Add(Me.txtpass)
+        Me.Controls.Add(Me.txtemail)
+        Me.Controls.Add(Me.txtid)
+        Me.Controls.Add(Me.lblcreatedby)
+        Me.Controls.Add(Me.lblcreatedat)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblcreatedat As Label
+    Friend WithEvents lblcreatedby As Label
+    Friend WithEvents txtid As TextBox
+    Friend WithEvents txtemail As TextBox
+    Friend WithEvents txtpass As TextBox
+    Friend WithEvents txtcreatedat As TextBox
+    Friend WithEvents txtcreatedby As TextBox
+    Friend WithEvents btnadd As Button
+    Friend WithEvents btnclear As Button
 End Class
--- src/VisualBasicWinFormsCoreApp/VisualBasicWinFormsCoreApp/Form1.vb
+++ src/VisualBasicWinFormsCoreApp/VisualBasicWinFormsCoreApp/Form1.vb
@@ -1,3 +1,38 @@
-﻿Public Class Form1
+﻿Imports System.IO
+Imports System.Data.SqlServerCe
+Imports VisualBasicWinFormsCoreApp.Data.Entities
 
+Public Class Form1
+
+    Private db As New NorthwindContext()
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadData()
+    End Sub
+
+    Public Sub LoadData()
+        Dim users = db.Users.ToList()
+        DataGridView1.DataSource = users
+    End Sub
+    Public Function Connection() As String
+        Dim domain = AppDomain.CurrentDomain.BaseDirectory
+        Dim db = Path.Combine(domain, "db.sdf")
+        If db Is Nothing Then
+            CreateDatabase(db)
+        End If
+        Dim cs = $"Data Source={db};Max Database Size=4091"
+        Return cs
+    End Function
+
+    Private Sub CreateDatabase(db As String)
+
+
+    End Sub
+
+    Private Sub DataGridView1_CellDoubleClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellDoubleClick
+        Dim obj = sender
+        Dim myevent = e
+        Dim idcol = DataGridView1.Columns("Id")
+        Dim idrow = DataGridView1.Rows.Item(0)
+
+    End Sub
 End Class
